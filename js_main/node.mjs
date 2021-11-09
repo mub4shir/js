37af1d598fb1a9f84954da290fe276d1f7852de6
mubashir <mubashir11131719@gmail.com>
2021-11-09 15:28:55 +0530

    EventEmitter

--- js_main/node.mjs
+++ js_main/node.mjs
@@ -1,13 +1,19 @@
 import { EventEmitter } from 'events';
+import express from 'express';
 const eventEmitter = new EventEmitter();
 
 // listen to the event
 
-eventEmitter.on('myEvent', () => {
-  console.log('Data Received');
-});
+// const app = express();
+// app.set('eventEmitter', eventEmitter);
 
-eventEmitter.emit('myEvent');
+// //access it from any module of the application
+
+// eventEmitter.on('myEvent', () => {
+//   console.log('Data Received');
+// });
+
+// eventEmitter.emit('myEvent');
 
 // eventEmitter.on('myEvent', () => {
 //   console.log('Listener 1');
@@ -18,3 +24,27 @@ eventEmitter.emit('myEvent');
 // eventEmitter.on('myEvent', () => {
 //   console.log('Listener 2');
 // });
+
+// console.log(app.get('eventEmitter'));
+
+// eventEmitter.on('myEvent', (data) => {
+//   console.log(data);
+// });
+
+// console.log('Statement A');
+// eventEmitter.emit('myEvent', 'Statement B');
+// console.log('Statement C');
+
+eventEmitter.on('myEvent', (data) => {
+  console.log(data, '- FIRST');
+});
+
+console.log('Statement A');
+
+eventEmitter.on('myEvent', (data) => {
+  console.log(data, '- SECOND');
+});
+
+eventEmitter.emit('myEvent', 'Emitted Statement');
+
+console.log('Statement B');
