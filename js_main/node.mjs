73d065cbd4e5eb08d60b72e81c5fec755664ba5d
mubashir <mubashir11131719@gmail.com>
2021-11-09 17:01:02 +0530

    EventEmitter

--- js_main/node.mjs
+++ js_main/node.mjs
@@ -1,6 +1,7 @@
 import { EventEmitter } from 'events';
 import express from 'express';
 import { createReadStream } from 'fs';
+import path from 'path';
 const eventEmitter = new EventEmitter();
 
 // listen to the event
@@ -62,20 +63,72 @@ const eventEmitter = new EventEmitter();
 
 // Where NodeJS Internally uses Event Emitters
 
-let chunkIndex = 0;
-const readStream = createReadStream('./data.txt');
+// let chunkIndex = 0;
+// const readStream = createReadStream('./data.txt');
 
-readStream.on('open', () => {
-  console.log('Started Reading...');
-});
+// readStream.on('open', () => {
+//   console.log('Started Reading...');
+// });
 
-readStream.on('end', () => {
-  console.log('Completed Reading...');
-});
+// readStream.on('end', () => {
+//   console.log('Completed Reading...');
+// });
 
-readStream.on('data', (chunk) => {
-  console.log('Chunk: ' + ++chunkIndex);
-  console.log('-----------------------------------------');
-  console.log(chunk);
-  console.log('\n');
-});
+// readStream.on('data', (chunk) => {
+//   console.log('Chunk: ' + ++chunkIndex);
+//   console.log('-----------------------------------------');
+//   console.log(chunk);
+//   console.log('\n');
+// });
+
+//console.log(import.meta.url);
+// process.on('exit', () => console.log('Exit'));
+// process.on('beforeExit', () => console.log('Before Exit'));
+// process.on('uncaughtException', () => {
+//   console.log('Exception');
+//   process.exit();
+// });
+// throw new Error('Test Error');
+
+// eventEmitter.on('myEvent', (data) => {
+//   console.log(data, '- ON');
+// });
+
+// eventEmitter.once('myEvent', (data) => {
+//   console.log(data, '- ONCE');
+// });
+
+// eventEmitter.emit('myEvent', 'Emitted Statement');
+// eventEmitter.emit('myEvent', 'Emitted Statement');
+// eventEmitter.emit('myEvent', 'Emitted Statement');
+
+// eventEmitter.on('myEvent', (data) => console.log(data, '- ON'));
+// eventEmitter.on('myEvent2', (data) => console.log(data, '- ON'));
+// eventEmitter.once('myEvent3', (data) => console.log(data, '- ONCE'));
+
+// console.log(eventEmitter.eventNames());
+// eventEmitter.emit('myEvent3', 'EVENT');
+// console.log(eventEmitter.eventNames());
+
+// removeListener()
+// function func1() {
+//   console.log('EVENT TRIGGERED');
+// }
+
+// eventEmitter.on('myEvent', func1);
+// eventEmitter.on('myEvent2', func1);
+
+// console.log(eventEmitter.eventNames());
+// eventEmitter.removeListener('myEvent', func1);
+// console.log(eventEmitter.eventNames());
+
+// removeAllListeners()
+function func1() {
+  console.log('EVENT TRIGGERED');
+}
+
+eventEmitter.on('myEvent', func1);
+eventEmitter.on('myEvent2', func1);
+
+eventEmitter.removeAllListeners();
+console.log(eventEmitter.eventNames());
